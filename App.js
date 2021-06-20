219dc5002a938670f45a9d71381b02b7e16c90c4
avneet1604 <avneet.devgan@gmail.com>
2021-06-20 15:17:29 +0530

    buzz app

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import InstagramScreen from "./screens/ins";
+import FacebookScreen from "./screens/fb";
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  Instagram: { screen: InstagramScreen },
+  Facebook: { screen: FacebookScreen }
+});
+
+const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
